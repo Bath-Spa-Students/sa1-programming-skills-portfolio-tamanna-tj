c1ec4ecac73389f6ef08e511698aa6fadd1919e5
tamanna-tj <uni.thamanna@gmail.com>
2024-11-09 00:27:31 +0400

    Biography

---
+++ Exercise3.py
@@ -0,0 +1,8 @@
+
+info = {
+    "name": "Thamanna Jannath",       
+    "hometown": "Ajman",  
+    "age": 19                 
+}
+
+print(f"Name: {info['name']}\nHometown: {info['hometown']}\nAge: {info['age']}")
