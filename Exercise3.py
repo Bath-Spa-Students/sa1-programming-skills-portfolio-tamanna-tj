022df9f6361a5bae81af1525793aeaf1604176ed
tamanna-tj <uni.thamanna@gmail.com>
2024-11-09 13:01:27 +0400

    Biography

--- Exercise3.py
+++ Exercise3.py
@@ -1,8 +1,20 @@
+#create a program that stores and prints your name, home town, and age to the console using a Python dictionary.
 
 info = {
     "name": "Thamanna Jannath",       
     "hometown": "Ajman",  
     "age": 19                 
 }
+print(info["name"])
+print(info["hometown"])
+print(info["age"])
 
-print(f"Name: {info['name']}\nHometown: {info['hometown']}\nAge: {info['age']}")
+#advanced requirement
+
+name=input("Enter your name:")
+hometown=input("Enter your hometown:")
+age=input("Enter your age:")
+info ={"name": name,"hometown":hometown,"age": age}
+print(info["name"])
+print(info["hometown"])
+print(info["age"])
