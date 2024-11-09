90e4f303d5da7c40fafba5380eacfe8ffc1a2289
tamanna-tj <uni.thamanna@gmail.com>
2024-11-09 12:14:27 +0400

    Delete Exercise3.py

--- Exercise3.py
+++
@@ -1,8 +0,0 @@
-
-info = {
-    "name": "Thamanna Jannath",       
-    "hometown": "Ajman",  
-    "age": 19                 
-}
-
-print(f"Name: {info['name']}\nHometown: {info['hometown']}\nAge: {info['age']}")
