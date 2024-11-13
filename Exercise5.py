685e1f19682d5a3ceacc8aac09ea36c3d1692e88
tamanna-tj <uni.thamanna@gmail.com>
2024-11-13 19:54:24 +0400

    Days of the Month

---
+++ Exercise5.py
@@ -0,0 +1,23 @@
+#a dictionary to map the month numbers (1-12) to the number of days in each month.
+
+days= {1: 31,2: 28,3: 31,4: 30,5: 31, 6: 30,7: 31,8: 31,9: 30, 10: 31,11: 30,12: 31}
+monthNumber = int(input("Enter the month number (1-12): "))
+if 1 <= monthNumber <= 12:
+        print(f"The month has {days[monthNumber]} days.")
+else:
+        print("Invalid month number. Please enter a valid number.")
+
+#Advanced Requirement
+
+days= {1: 31,2: 28,3: 31,4: 30,5: 31, 6: 30,7: 31,8: 31,9: 30, 10: 31,11: 30,12: 31}
+monthNumber = int(input("Enter the month number (1-12): "))
+if 1 <= monthNumber <= 12:
+        if monthNumber==2:
+                leapyear=input("Checking for leap year?:")
+
+                if leapyear=="Yes":
+                        print("The month has 29 days")
+
+                else:
+                        print("The month has 28 days")
+
