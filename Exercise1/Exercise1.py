ab3c94642af6914760ecb38647011d07037bbaa7
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 01:10:07 +0400

    Coding is Cool

--- Exercise1/Exercise1.py
+++ Exercise1/Exercise1.py
@@ -1,5 +1,10 @@
+# To output the phrase “Coding is Cool” to the console using variables and string concatenation.
+
 word1=("coding")
 word2=("is")
 word3=("cool")
+
+# Use string concatenation to combine the variables and print the phrase
+
 word4=(word1+word2+word3)
 print(word4)
