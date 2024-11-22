0d9a1bc79645a86d3d61ac02ee3a08d67bf07eb2
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 12:45:42 +0400

    List - Names

---
+++ Bonus exercises/List -Names.py
@@ -0,0 +1,12 @@
+#List -Names  
+#Store the names of a few of your friends in a list called names. Print each person’s name 
+#by accessing each element in the list, one at a time.
+
+#Store friends names in the list
+names = ["Asinsha","Athul","Wazeema","Thamanna"]
+
+#Prints each name at a time
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
