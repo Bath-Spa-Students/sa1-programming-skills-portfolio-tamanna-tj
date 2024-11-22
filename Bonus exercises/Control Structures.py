c37803120c46151b6ec67e269f8467a012adfaf5
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 13:03:37 +0400

    Control Structures

---
+++ Bonus exercises/Control Structures.py
@@ -0,0 +1,21 @@
+#Control Structures: 
+#Imagine an alien was just shot down in a game. Create a variable called alien_color and 
+#assign it a value of 'green', 'yellow', or 'red'. 
+#•Write an if statement to test whether the alien’s color is green. If it is, print a message 
+#that the player just earned 5 points. 
+#•Write one version of this program that passes the if test and another that fails. (The 
+#version that fails will have no output.)
+
+# Assigning the alien's color green
+alien_color = "green"
+
+# If statement to check the alien's color
+if alien_color == "green" :
+    print("You earned 5 point!")
+
+# Assigning the alien's color yellow
+alien_color = "yellow"
+
+# If statement to check the alien's color
+if alien_color == "green" :
+    print("You earned 5 point!")
