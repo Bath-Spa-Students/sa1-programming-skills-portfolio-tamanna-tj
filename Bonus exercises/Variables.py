677d9e22fe8ea0387acc69aaf93a7b368b6d8e3f
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 12:33:13 +0400

    Variables

---
+++ Bonus exercises/Variables.py
@@ -0,0 +1,11 @@
+#Variables: 
+#Assign a message to a variable, and print that message. Then change the value of the 
+#variable to a new message, and print the new message.
+
+#Assigning a variable called text and value.
+text_1 = "This is Code Lab!"
+print(text_1) #to display the first value
+
+#Assigning  different value to the same variable.
+text_1 = "This is Python!"
+print(text_1) #To display the new value
