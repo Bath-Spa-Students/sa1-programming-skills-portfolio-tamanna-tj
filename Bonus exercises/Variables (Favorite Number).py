2c695a25590696fe54b0f98b9584ac4703094a27
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 12:39:09 +0400

    Variables (Favorite Number)

---
+++ Bonus exercises/Variables (Favorite Number).py
@@ -0,0 +1,12 @@
+#Variables (Favorite Number): 
+#Use a variable to represent your favorite number. Then, using that variable, create a 
+#message that reveals your favorite number. Print that message.
+
+#To show your favorite number
+fav_num = 6
+
+#To reaveal your favorite number
+number = f"My favorite number is {fav_num}."
+
+#To display the number
+print(number)
