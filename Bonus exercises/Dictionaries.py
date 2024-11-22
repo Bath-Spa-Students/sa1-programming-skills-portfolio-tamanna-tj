4e79ffbd72c362e133f8c1eab562ceee2797db12
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 13:10:16 +0400

    Dictionaries

---
+++ Bonus exercises/Dictionaries.py
@@ -0,0 +1,19 @@
+#Dictionaries: 
+#Use a dictionary to store information about a person you know.Store their first name, 
+#last name, age, and the city in which they live.  
+#You should have keys such as first_name, last_name, age, and city. Print each piece of 
+#information stored in your dictionary.
+
+#Creating the dictionary
+info = {
+    "first_name": "Thamanna",
+    "last_name": "Jannath",
+    "age": 19,
+    "city" : "Ajman"
+}
+
+#Printing each piece of information
+print(f"First Name: {info['first_name']}")
+print(f"Last Name: {info['last_name']}")
+print(f"Age: {info['age']}")
+print(f"City: {info['city']}")
