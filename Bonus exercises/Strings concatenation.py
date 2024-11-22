b1dce63d45c6bd397b2eb911b37320e10d9ffcd7
tamanna-tj <uni.thamanna@gmail.com>
2024-11-22 12:24:52 +0400

    Strings Concatenation

---
+++ Bonus exercises/Strings concatenation.py
@@ -0,0 +1,13 @@
+#Strings Concatenation: 
+#Write three strings in different variables and print the output as one string. 
+
+#assigning the one word to the variable word1
+word1 = "Come"
+
+#assigning the one word to the variable word2
+word2 = "to"
+
+#assigning the one word to the varibale word3
+word3 = "Dubai"
+output = word1 + " " + word2 + " " + word3
+print(output)
